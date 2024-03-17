d2b4944c8ec71e92f805f22fd7385193ef763c55
or-feus <barca105@nate.com>
2024-03-17 16:57:45 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject, http::GraphiQLSource};
+use async_graphql::http::GraphiQLSource;
+use async_graphql::*;
 use async_graphql_axum::GraphQL;
 use axum::{
     response::{self, IntoResponse},
@@ -11,25 +12,30 @@ use tokio::net::TcpListener;
 struct MyObject {
     a: i32,
     b: i32,
-
     c: i32,
 }
 
-
+struct User {
+    a: i32,
+    b: i32,
+    c: i32,
+}
 
 struct Query;
 #[Object]
 impl Query {
-    async fn user(&self, a: i32) -> Result<Option<MyObject>, anyhow::Error> {
-        Ok(Some(MyObject {
-            a,
-            b: 10,
-            c: 20,
-        }))
+    async fn user(&self, a: i32) -> Result<Option<MyObject>> {
+        Ok(Some(MyObject { a, b: 10, c: 20 }))
+    }
+
+    async fn context<'ctx>(&self, ctx: &Context<'ctx>) -> Result<&'ctx String> {
+        ctx.data::<String>()
+    }
+
+    async fn test(&self) -> Result<String> {
+        Ok("Hello, World!".to_string())
     }
 }
-
-
 
 async fn graphiql() -> impl IntoResponse {
     response::Html(GraphiQLSource::build().endpoint("/").finish())
@@ -37,10 +43,12 @@ async fn graphiql() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data("Context data".to_string())
+        .finish();
 
-    let app = Router::new()
-        .route("/", get(graphiql).post_service(GraphQL::new(schema)));
+    println!("{}", &schema.sdl());
+    let app = Router::new().route("/", get(graphiql).post_service(GraphQL::new(schema)));
 
     println!("Playground: http://localhost:8000");
 
